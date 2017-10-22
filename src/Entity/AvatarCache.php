6ca563e427b36bb5940ae547714bbd7dfb011738
Daniel Phin <pro@danielph.in>
2017-10-22 20:07:04 +0800

    Added avatars_avatar_cache entity type.

---
+++ src/Entity/AvatarCache.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\avatars\Entity;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+
+/**
+ * Defines the Avatar cache entity.
+ *
+ * @ContentEntityType(
+ *   id = "avatars_avatar_cache",
+ *   label = @Translation("Avatar cache"),
+ *   base_table = "avatars_avatar_cache",
+ *   entity_keys = {
+ *     "id" = "acid",
+ *     "bundle" = "avatar_service",
+ *     "uuid" = "uuid"
+ *   },
+ *   bundle_entity_type = "avatars_service"
+ * )
+ */
+class AvatarCache extends ContentEntityBase implements AvatarCacheInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    // @see \Drupal\Core\Field\Plugin\Field\FieldType\StringItem
+    $fields['identifier'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Identifier'))
+      ->setDescription(t('The identifier used to generate the avatar.'))
+      ->setRequired(TRUE)
+      ->setReadOnly(TRUE);
+
+    // @see \Drupal\dynamic_entity_reference\Plugin\Field\FieldType\DynamicEntityReferenceItem
+    $fields['entity'] = BaseFieldDefinition::create('dynamic_entity_reference')
+      ->setLabel(t('Entity'))
+      ->setDescription(t('The entity the avatar is based on.'))
+      ->setRequired(FALSE);
+
+    // @see \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem
+    $fields['avatar'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Reference to a file entity containing an image.'))
+      ->setSetting('target_type', 'file')
+      ->setCardinality(1)
+      ->setRequired(FALSE);
+
+    // @see \Drupal\Core\Field\Plugin\Field\FieldType\CreatedItem
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Date of creation'))
+      ->setDescription(t('The date the avatar cache was created.'))
+      ->setRequired(TRUE)
+      ->setReadOnly(TRUE);
+
+    // @see \Drupal\Core\Field\Plugin\Field\FieldType\TimestampItem
+    $fields['last_check'] = BaseFieldDefinition::create('timestamp')
+      ->setLabel(t('Date of last check.'))
+      ->setDescription(t('Date the avatar was last checked.'))
+      ->setRequired(TRUE);
+
+    return $fields;
+  }
+
+}
---
+++ src/Entity/AvatarCacheInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\avatars\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface for Avatar cache entities.
+ */
+interface AvatarCacheInterface extends ContentEntityInterface {
+
+}
